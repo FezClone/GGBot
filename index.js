ea5b64f440dfc6b7805b60f1774d4df2b35cc759
JimJamPieMan <rudlandjames@gmail.com>
2018-06-04 22:10:39 +0930

    Warns
    -- Added warns and revoke warns

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const YTDL = require("ytdl-core");
 const PREFIX = ("gg");
+const shortid = require('shortid');
+const fs = require("fs");
 
 
 bot.on("guildMemberAdd", member => { 
@@ -13,6 +15,31 @@ bot.on("guildMemberAdd", member => {
     member.addRole(role)
 
 });
+
+bot.on("guildCreate", guild=>{
+
+
+    var json = {
+        "serverData": {
+            "warns": []
+        }
+    };
+    json = JSON.stringify(json);
+    fs.writeFileSync('./' + guild.id + '.json', json, (err) => {
+        if (!err) {
+            console.log('done');
+        } else if (err) {
+            console.log(err);
+        }
+    });
+});
+
+//When the bot leaves a server delete the server settings
+bot.on("guildDelete", guild => {
+    var serverDataFile = './' + guild.id + '.json';
+    fs.unlinkSync(serverDataFile);
+});
+
 
 
 //bot.on("message", (message) => { 
@@ -46,7 +73,147 @@ bot.on("message", function(message) {
   const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
 
 switch (args[0].toLowerCase()) {
-    
+   
+case "revokewarn":
+
+        
+       
+            
+        var serverDataFile = './' + message.guild.id + '.json';
+        var revokedWarn = false;
+        var id = args[1];
+        if (!args[1]) {
+            message.channel.send("No ID given to delete");
+            return;
+        }
+
+
+        fs.readFile(serverDataFile, 'utf-8', (err, data) => {
+            if (err) throw err;
+
+            var obj = JSON.parse(data);
+
+            for (var i = 0; i < obj.serverData.warns.length; i++) {
+                if (obj.serverData.warns[i].warnID == id) {
+
+                    obj.serverData.warns.splice(i, 1);
+                    revokedWarn = true;
+                    message.channel.send("Warning revoked");
+
+
+                }
+            }//now it an object
+            if (revokedWarn == false) {
+                message.channel.send("An error happened. (Invalid ID)");
+                return;
+            }
+
+
+            //add some data
+            var json = JSON.stringify(obj);
+            fs.writeFile(serverDataFile, json, 'utf8', (err) => {
+                if (err) throw err;
+            });
+
+
+        });
+        break;
+case "warn":
+
+       
+
+        var serverDataFile = './' + message.guild.id + '.json';
+
+        //check user
+        //reason
+        var user = message.mentions.members.first();
+
+
+        if (!user) {
+            message.channel.send("please mention a valid user");
+            return;
+        }
+        args.shift();
+        if (!args[1]) {
+            message.channel.send("please give a reason");
+            return;
+        }
+
+        var warnid = shortid.generate();
+        args.shift();
+        var reason = args.join(" ");
+
+
+        fs.readFile(serverDataFile, 'utf-8', (err, data) => {
+            if (err) throw err;
+
+            var warning = {
+                warnID: warnid,
+                person: user.id,
+                reason: reason
+
+            }
+
+
+            var obj = JSON.parse(data); //now it an object
+            obj.serverData.warns.push(warning);
+
+            //add some data
+            var json = JSON.stringify(obj);
+            fs.writeFile(serverDataFile, json, 'utf8', (err) => {
+                if (err) throw err;
+            });
+
+
+        });
+        var data = fs.readFileSync(serverDataFile, 'utf-8');
+        var amountOfWarns = 1;
+        data = JSON.parse(data);
+
+
+
+        for (var i = 0; i < data.serverData.warns.length; i++) {
+            if (data.serverData.warns[i].person == user.id) {
+                amountOfWarns++;
+
+
+            }
+        }
+        if (amountOfWarns >= 6) {
+            user.ban(reason);
+            message.channel.send("User Banned");
+
+        } else if (amountOfWarns >= 5) {
+            message.channel.send(user + " One more warn and you are Banned");
+
+        } else if (amountOfWarns >= 3) {
+            user.kick(reason);
+            message.channel.send("User Kicked");
+        } else if (amountOfWarns >= 2) {
+            message.channel.send(user + " One more warn and you are kicked");
+
+        }
+
+        const embed = {
+            "title": "Warning given to " + user.user.username,
+            "color": 9442302,
+            "fields": [{
+                name: "UserId: ",
+                value: user.id
+            }, {
+                name: "WarnID: ",
+                value: warnid
+            }, {
+                name: "Reason: ",
+                value: reason
+            }
+            ]
+        };
+        message.channel.send({
+            embed
+        });
+    break;
+
 case "priv":
 message.guild.createRole({
     name: 'entry',
@@ -109,4 +276,4 @@ case "play":
 
 //bot.login('no');
 
-bot.login('no');
+bot.login('NDMwOTgzMTI1NzQxMzM4NjI0.DaYKBQ.3tAqcxu0Ypn6d8eA_aIxqE7OFzY');
